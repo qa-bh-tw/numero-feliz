0ce14b6dfd2bd78cf33da091e36afcadb3af9ce4
Fábio Pio <fpio@thoughtworks.com>
2016-01-25 20:11:33 -0200

    refatora conversao

--- src/main/java/com/felicidade/QuebradorDeNumero.java
+++ src/main/java/com/felicidade/QuebradorDeNumero.java
@@ -1,17 +1,20 @@
 package com.felicidade;
 
+import static java.lang.Character.getNumericValue;
+
 public class QuebradorDeNumero {
 
     public int[] quebraNumero(int numero) {
         if (numero <= 0 ){
-           throw new IllegalArgumentException("Numero deve ser maior que nove");
+           throw new IllegalArgumentException("Numero deve ser maior que zero");
         }
-        int tamanhoVetor = String.valueOf(numero).length();
+
+        String numeroEmString = String.valueOf(numero);
+        int tamanhoVetor = numeroEmString.length();
         int[] resultado = new int[tamanhoVetor];
-        String valorString = String.valueOf(numero);
-        int i = valorString.length();
-        for (int j = 0; j < i; j++) {
-            resultado[j] = Integer.parseInt(valorString.substring(j, j + 1));
+
+        for (int posicao = 0; posicao < tamanhoVetor; posicao++) {
+            resultado[posicao] = getNumericValue(numeroEmString.charAt(posicao));
         }
         return resultado;
     }
